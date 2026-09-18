5d954a95d876339cbb20a67b24e4aaf80e8426aa
Dien Gia Khiem <25AD11006@hcmute.edu.vn>
2026-09-18 09:53:21 +0700

    Update

--- PracticesString2.cpp
+++ PracticesString2.cpp
@@ -3,15 +3,42 @@
 using namespace std;
 
 int main(){
+    //step 1
     string s1 = "Hello";
     string s2 = "C++";
     cout << s1 << endl;
     cout << s2 << endl;
 
+    //step 2
     string name;
     cout << "Enter your name: ";
     getline(cin, name);
     cout << "Hello, " << name << "!" << endl;
+
+    //step 3
+    string first = "Hello";
+    string second = "World";
+    string message = first + " " + second;
+    cout << message << endl;
+
+    //step 4
+    string s = "Hello C++";
+    cout << "Length of string: " << s.length() << endl;
+
+    //step 5
+    string s = "Hello C++";
+    cout << "First character: " << s[0] << endl;
+    cout << "Third character: " << s[2] << endl;
+
+    //step 6
+    string text = "I love C++";
+    size_t pos = text.find("C++");
+    if (pos != string::npos) {
+        cout << "'C++' found at position: " << pos << endl;
+    } else {
+        cout << "'C++' not found in the text." << endl;
+    }
+
     return 0;
 }
  
