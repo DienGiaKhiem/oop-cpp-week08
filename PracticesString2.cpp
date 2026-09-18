925cae9bb945c974fb087df8e02c0be56afce457
Dien Gia Khiem <25AD11006@hcmute.edu.vn>
2026-09-18 09:16:11 +0700

    Update

---
+++ PracticesString2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string s1 = "Hello";
+    string s2 = "C++";
+    cout << s1 << endl;
+    cout << s2 << endl;
+
+    string name;
+    cout << "Enter your name: ";
+    getline(cin, name);
+    cout << "Hello, " << name << "!" << endl;
+    return 0;
+}
+ 
